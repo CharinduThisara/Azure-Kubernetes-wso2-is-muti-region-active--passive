193c42daeb88f79fb82d40ed5b669f7dad6c2d90
CharinduThisara <thisaraimc@gmail.com>
2024-03-01 15:22:49 +0530

    Update switchReadOnly.sh and is_node.yaml

--- Scripts/switchReadOnly.sh
+++ Scripts/switchReadOnly.sh
@@ -17,4 +17,5 @@ sed -i "s/\(^\s*registry_mode\s*:\s*\).*\$/\1$NEW_REGISTRY_MODE/" "$VALUES_FILE"
 
 echo "Updated registry_mode to $NEW_REGISTRY_MODE in values.yaml"
 
-helm upgrade wso2-is-ha ../wso2-kubecluster-ha-mode 
+helm upgrade wso2-is-ha . --values ./values.yaml --recreate-pods
+
