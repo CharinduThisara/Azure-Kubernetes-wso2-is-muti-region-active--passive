afd89bb7bbf6725ea93240c871da90342e9e7805
CharinduThisara <thisaraimc@gmail.com>
2024-03-04 10:27:09 +0530

    Fix file names in switchReadOnly.sh script

--- Scripts/switchReadOnly.sh
+++ Scripts/switchReadOnly.sh
@@ -3,11 +3,11 @@
 # Set the desired value for registry_mode (true or false)
 if [ "$1" == "ro" ]; then
     rm -f ../wso2-kubecluster-ha-mode/templates/is_deployment.yaml
-    cp ../wso2-kubecluster-ha-mode/deployments/is-node_RO.yaml ../wso2-kubecluster-ha-mode/templates/is_deployment.yaml
+    cp ../wso2-kubecluster-ha-mode/deployments/is_node_RO.yaml ../wso2-kubecluster-ha-mode/templates/is_deployment.yaml
     helm upgrade wso2-is-ha ../wso2-kubecluster-ha-mode
 elif [ "$1" == "rw" ]; then
     rm -f ../wso2-kubecluster-ha-mode/templates/is_deployment.yaml
-    cp ../wso2-kubecluster-ha-mode/deployments/is-node_RW.yaml ../wso2-kubecluster-ha-mode/templates/is_deployment.yaml
+    cp ../wso2-kubecluster-ha-mode/deployments/is_node_RW.yaml ../wso2-kubecluster-ha-mode/templates/is_deployment.yaml
     helm upgrade wso2-is-ha ../wso2-kubecluster-ha-mode
 else
     echo "Error: Invalid argument. Please specify ro or rw."
